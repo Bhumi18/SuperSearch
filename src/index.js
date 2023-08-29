abede999c18485d812df658931a7b1092789230a
Bhumi18 <bhumisadariya18@gmail.com>
2023-08-29 12:27:15 +0530

    chatwidget moved into app.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import ChatWidget from "./components/ChatWidget";
+// import ChatWidget from "./components/ChatWidget";
 // import "./index.css";
 
 // Opt-in to Webpack hot module replacement
 if (module.hot) module.hot.accept();
 
 /* eslint-disable no-undef */
-ReactDOM.render(<ChatWidget />);
+ReactDOM.render(<App />);
 
-export { ChatWidget };
+// export { ChatWidget };
