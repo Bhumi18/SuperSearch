c2311fa8463c5f68da5e9e4c0938ceda97fef3e4
Bhumi18 <bhumisadariya18@gmail.com>
2023-08-29 15:08:34 +0530

    root element removed

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 // import ChatWidget from "./components/ChatWidget";
 // import "./index.css";
+function index() {
+  // Opt-in to Webpack hot module replacement
+  if (module.hot) module.hot.accept();
 
-// Opt-in to Webpack hot module replacement
-if (module.hot) module.hot.accept();
+  /* eslint-disable no-undef */
 
-/* eslint-disable no-undef */
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-reportWebVitals();
-
+  <App />;
+}
+export default index;
 // export { ChatWidget };
